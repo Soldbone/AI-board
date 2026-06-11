27a4fd79ce902b9154f572dad93eef017b58f161
이규정 <veracro97@gmail.com>
2026-06-11 13:57:16 +0900

    feat: 게시글 생성 라우트 입구 추가

--- kyujeong/backend/src/posts/posts.service.ts
+++ kyujeong/backend/src/posts/posts.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  create(createPostDto: CreatePostDto) {
+    return createPostDto;
+  }
 }
